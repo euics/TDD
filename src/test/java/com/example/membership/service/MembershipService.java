273ed883d5ff92c1d1ee8a6646b16ef8b05e2f9f
euics <pjh575157@gmail.com>
2024-04-04 23:50:07 +0900

    test: 멤버십등록성공 코드 개발

--- src/test/java/com/example/membership/service/MembershipService.java
+++ src/test/java/com/example/membership/service/MembershipService.java
@@ -3,6 +3,7 @@ package com.example.membership.service;
 import com.example.membership.domain.Membership;
 import com.example.membership.exception.MembershipException;
 import com.example.membership.repository.MembershipRepository;
+import com.example.membership.service.vo.MembershipResponse;
 import com.example.membership.util.MembershipErrorResult;
 import com.example.membership.util.MembershipType;
 import lombok.RequiredArgsConstructor;
@@ -13,13 +14,24 @@ import org.springframework.stereotype.Service;
 public class MembershipService {
     private final MembershipRepository membershipRepository;
 
-    public Membership addMembership(final String userId, final MembershipType membershipType, final Integer point) {
+    public MembershipResponse addMembership(final String userId, final MembershipType membershipType, final Integer point) {
         final Membership result = membershipRepository.findByUserIdAndMembershipType(userId, membershipType);
 
         if(result != null) {
             throw new MembershipException(MembershipErrorResult.DUPLICATE_MEMBERSHIP_REGISTER);
         }
 
-        return null;
+        final Membership membership = Membership.builder()
+                .userId(userId)
+                .point(point)
+                .membershipType(membershipType)
+                .build();
+
+        final Membership savedMembership = membershipRepository.save(membership);
+
+        return MembershipResponse.builder()
+                .id(savedMembership.getId())
+                .membershipType(savedMembership.getMembershipType())
+                .build();
     }
 }
--- src/test/java/com/example/membership/service/test/MembershipServiceTest.java
+++ src/test/java/com/example/membership/service/test/MembershipServiceTest.java
@@ -4,6 +4,7 @@ import com.example.membership.domain.Membership;
 import com.example.membership.exception.MembershipException;
 import com.example.membership.repository.MembershipRepository;
 import com.example.membership.service.MembershipService;
+import com.example.membership.service.vo.MembershipResponse;
 import com.example.membership.util.MembershipErrorResult;
 import com.example.membership.util.MembershipType;
 import org.junit.jupiter.api.DisplayName;
@@ -15,7 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class MembershipServiceTest {
@@ -39,5 +40,33 @@ public class MembershipServiceTest {
 
         // then
         assertThat(result.getErrorResult()).isEqualTo(MembershipErrorResult.DUPLICATE_MEMBERSHIP_REGISTER);
+    }
+
+    @Test
+    @DisplayName("멤버십등록성공")
+    public void 멤버십등록성공() {
+        // given
+        doReturn(null).when(membershipRepository).findByUserIdAndMembershipType(userId, membershipType);
+        doReturn(membership()).when(membershipRepository).save(any(Membership.class));
+
+        // when
+        final MembershipResponse result = target.addMembership(userId, membershipType, point);
+
+        // then
+        assertThat(result.getId()).isNotNull();
+        assertThat(result.getMembershipType()).isEqualTo(MembershipType.NAVER);
+
+        // verify
+        verify(membershipRepository, times(1)).findByUserIdAndMembershipType(userId, membershipType);
+        verify(membershipRepository, times(1)).save(any(Membership.class));
+    }
+
+    private Membership membership() {
+        return Membership.builder()
+                .id(-1L)
+                .userId(userId)
+                .point(point)
+                .membershipType(MembershipType.NAVER)
+                .build();
     }
 }
